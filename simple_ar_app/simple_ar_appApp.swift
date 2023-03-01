74c32b451e864ba9c6a249c114f82a9efc1e13f7
Greg Ramos <grr5000@live.com>
2023-02-28 19:57:22 -0500

    Initial Commit

---
+++ simple_ar_app/simple_ar_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_ar_appApp.swift
+//  simple_ar_app
+//
+//  Created by Greg Ramos on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct simple_ar_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
